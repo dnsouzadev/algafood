95ba11daed7b3e1cfb4b1db761a8db690928984d
Daniel Souza <workdndsza@gmail.com>
2025-01-15 16:55:15 -0300

    criando pedido e statuspedido e suas migracoes

---
+++ src/main/java/com/dnsouzadev/algafood/domain/model/ItemPedido.java
@@ -0,0 +1,32 @@
+package com.dnsouzadev.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+
+}
