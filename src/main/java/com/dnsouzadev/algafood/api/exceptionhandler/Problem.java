eb0849a9e31ed346555fdd98eb9d02b047607527
Daniel Souza <workdndsza@gmail.com>
2025-01-23 16:14:04 -0300

    estendendo o formato do problema e tratando todas a excecoes nao capturadas

--- src/main/java/com/dnsouzadev/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/dnsouzadev/algafood/api/exceptionhandler/Problem.java
@@ -3,54 +3,36 @@ package com.dnsouzadev.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @JsonInclude(Include.NON_NULL)
 public class Problem {
 
     private Integer status;
+    private LocalDateTime timestamp;
     private String type;
     private String title;
     private String detail;
+    private String userMessage;
 
-    public static class builder {
-        private Integer status;
-        private String type;
-        private String title;
-        private String detail;
-
-        public builder status(Integer status) {
-            this.status = status;
-            return this;
-        }
-
-        public builder type(String type) {
-            this.type = type;
-            return this;
-        }
-
-        public builder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public builder detail(String detail) {
-            this.detail = detail;
-            return this;
-        }
-
-        public Problem build() {
-            Problem problem = new Problem();
-            problem.status = status;
-            problem.type = type;
-            problem.title = title;
-            problem.detail = detail;
-
-            return problem;
-        }
+    private Problem(Builder builder) {
+        this.status = builder.status;
+        this.timestamp = builder.timestamp;
+        this.type = builder.type;
+        this.title = builder.title;
+        this.detail = builder.detail;
+        this.userMessage = builder.userMessage;
     }
 
     public Integer getStatus() {
         return status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 
     public String getType() {
@@ -63,5 +45,56 @@ public class Problem {
 
     public String getDetail() {
         return detail;
+    }
+
+    public String getUserMessage() {
+        return userMessage;
+    }
+
+    public static class Builder {
+        private Integer status;
+        private LocalDateTime timestamp;
+        private String type;
+        private String title;
+        private String detail;
+        private String userMessage;
+
+        public Builder status(Integer status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder timestamp(LocalDateTime timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder detail(String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public Builder userMessage(String userMessage) {
+            this.userMessage = userMessage;
+            return this;
+        }
+
+        public Problem build() {
+            return new Problem(this);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
     }
 }
--- src/main/java/com/dnsouzadev/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/dnsouzadev/algafood/api/exceptionhandler/ProblemType.java
@@ -2,13 +2,15 @@ package com.dnsouzadev.algafood.api.exceptionhandler;
 
 public enum ProblemType {
 
-    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensivel"),
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
 
-    private final String title;
-    private final String uri;
+    private String title;
+    private String uri;
 
     ProblemType(String path, String title) {
         this.uri = "https://algafood.com.br" + path;
