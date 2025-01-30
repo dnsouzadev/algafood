bc847318802c57a8c4c63c6383981b334c867e00
Daniel Souza <workdndsza@gmail.com>
2025-01-30 14:51:00 -0300

    configurando um banco de dados para teste e usando properties propria

--- src/test/java/com/dnsouzadev/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/dnsouzadev/algafood/CadastroCozinhaIT.java
@@ -2,22 +2,34 @@ package com.dnsouzadev.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 
     @LocalServerPort
     private int port;
 
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
                 .when()
                 .get()
@@ -28,13 +40,23 @@ class CadastroCozinhaIT {
     @Test
     public void deveConterQuatroCozinhas_QuandoConsultarCozinhas() {
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
                 .when()
                 .get()
                 .then()
                 .body("", Matchers.hasSize(4))
                 .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
+
+    @Test
+    public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+        RestAssured.given()
+                .body("{ \"nome\": \"Chinesa\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(201);
     }
 }
