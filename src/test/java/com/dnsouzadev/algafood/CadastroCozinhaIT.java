c8452c81e7cf10afa55a5006b53d8ace6d5fedca
Daniel Souza <workdndsza@gmail.com>
2025-01-30 14:34:50 -0300

    validando o corpo da resposta http

--- src/test/java/com/dnsouzadev/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/dnsouzadev/algafood/CadastroCozinhaIT.java
@@ -2,6 +2,7 @@ package com.dnsouzadev.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -13,7 +14,7 @@ class CadastroCozinhaIT {
     private int port;
 
     @Test
-    public void deveRetornarStatus200_QuandoConsultarCozinha() {
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.given()
                 .basePath("/cozinhas")
                 .port(port)
@@ -24,4 +25,16 @@ class CadastroCozinhaIT {
                 .statusCode(200);
     }
 
+    @Test
+    public void deveConterQuatroCozinhas_QuandoConsultarCozinhas() {
+        RestAssured.given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("", Matchers.hasSize(4))
+                .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
 }
