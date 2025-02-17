85b58d1b1ec2d18850159f6bd2d7ebe908dab07d
Daniel Souza <workdndsza@gmail.com>
2025-02-17 16:50:44 -0300

    criando o validator para o content-type do arquivo de upload

--- src/main/java/com/dnsouzadev/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/dnsouzadev/algafood/api/model/input/FotoProdutoInput.java
@@ -1,14 +1,17 @@
 package com.dnsouzadev.algafood.api.model.input;
 
+import com.dnsouzadev.algafood.core.validation.FileContentType;
 import com.dnsouzadev.algafood.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
     private MultipartFile arquivo;
     @NotBlank
     private String descricao;
