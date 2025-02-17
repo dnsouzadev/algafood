186fedb9b5396999b028b5a49aa4e4ea9f80d131
Daniel Souza <workdndsza@gmail.com>
2025-02-17 16:47:09 -0300

    criando uma anotacao para validacao de tamanho do upload do arquivo

--- src/main/java/com/dnsouzadev/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/dnsouzadev/algafood/api/model/input/FotoProdutoInput.java
@@ -1,5 +1,6 @@
 package com.dnsouzadev.algafood.api.model.input;
 
+import com.dnsouzadev.algafood.core.validation.FileSize;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,6 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FotoProdutoInput {
 
     @NotNull
+    @FileSize(max = "500KB")
     private MultipartFile arquivo;
     @NotBlank
     private String descricao;
