5ea3df3ded32dec36c91caf731b040fd27f36805
Daniel Souza <workdndsza@gmail.com>
2025-02-20 17:30:34 -0300

    impl: remocao de arquivo no storage s3

--- src/main/java/com/dnsouzadev/algafood/domain/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/dnsouzadev/algafood/domain/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,6 +2,7 @@ package com.dnsouzadev.algafood.domain.infrastructure.service.storage;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.dnsouzadev.algafood.core.storage.StorageProperties;
@@ -50,7 +51,16 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+        try {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
+            var deleteObject = new DeleteObjectRequest(
+                    storageProperties.getS3().getBucket(), caminhoArquivo);
+
+            amazonS3.deleteObject(deleteObject);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+        }
     }
 
     private String getCaminhoArquivo(String nomeArquivo) {
