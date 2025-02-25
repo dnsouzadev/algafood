b739a664152b5224d9c37333051216d956293275
Daniel Souza <workdndsza@gmail.com>
2025-02-25 18:38:57 -0300

    impl: openApi docs para documentar via json e via swagger

---
+++ src/main/java/com/dnsouzadev/algafood/core/openapi/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.dnsouzadev.algafood.core.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Algafood API")
+                        .description("API aberta para clientes e restaurantes")
+                        .version("1")
+                        .contact(new Contact()
+                                .name("Daniel Souza")
+                                .email("contato@dnsouzadev.tech")
+                        ));
+    }
+}
